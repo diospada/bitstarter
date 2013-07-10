163f658c88264cbaec508ba3aac4e8c827da8fda
root <root@alberto-VirtualBox.(none)>
2013-07-10 21:01:32 +0200

    hello world form file

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fst = require('fs');
+
+var text = fst.readFileSync('index.html','utf8');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(text.toString());
 });
 
 var port = process.env.PORT || 5000;
